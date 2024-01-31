67ea2f2930f5ce461a21d835748c5d24b6ab6583
NeilAnayaC <122710580+NeilAnayaC@users.noreply.github.com>
2024-01-31 18:10:10 -0500

    add

--- MSSeguridad/MSSeguridad/src/main/java/TALLER/MSSeguridad/repository/IAuthRepositoryUsu.java
+++ MSSeguridad/MSSeguridad/src/main/java/TALLER/MSSeguridad/repository/IAuthRepositoryUsu.java
@@ -1,6 +1,6 @@
 package TALLER.MSSeguridad.repository;
 
-import java.util.Optional;
+//import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
